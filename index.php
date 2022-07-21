9fa484c3a79395f904d9f630c5ecf9db70709833
BrunoSilva1204 <bruno1204998@gmail.com>
2022-07-21 23:16:24 +0100

    codimarc exem

---
+++ index.php
@@ -0,0 +1,96 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+
+  </head>
+<body>
+
+<!------------------------------------>
+<header class="main-header">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg main-nav px-0">
+        <a class="navbar-brand" href="/mojo">Codimarc</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainMenu" aria-controls="mainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar icon-bar-1"></span>
+                        <span class="icon-bar icon-bar-2"></span>
+                        <span class="icon-bar icon-bar-3"></span>
+                    </button>
+        <div class="collapse navbar-collapse" id="mainMenu">
+          <ul class="navbar-nav ml-auto text-uppercase f1">
+            <li>
+              <a href="#home" class="active active-first">Calcular etiquetas</a>
+            </li>
+            <!--<li>
+              <a href="#about">about us</a>
+            </li>
+            <li>
+              <a href="#service">services</a>
+            </li>
+            <li>
+              <a href="#project">projects</a>
+            </li>
+            <li>
+              <a href="#team">team</a>
+            </li>
+            <li>
+              <a href="#testimony">testimonils</a>
+            </li>
+            <li>
+              <a href="#contact">contact</a>
+            </li>-->
+          </ul>
+        </div>
+      </nav>
+    </div>
+    <!-- /.container -->
+  </header>
+<!------------------------------------>
+
+<div class="container my_container">
+    <form>
+        <div class="row">
+            <div class="mb-5 col-4">
+              <label class="form-label">Total etiquetas</label>
+              <input type="number" class="form-control" id="total_etiquetas" value="1000">
+            </div>
+            <div class="mb-5 col-4">
+              <label class="form-label">Total vias do cortante</label>
+              <input type="number" class="form-control" id="vias_cortante" value="5">
+            </div>
+            <div class="mb-5 col-4">
+              <label class="form-label">Total vias Reais</label>
+              <input type="number" class="form-control" id="vias_usadas" value="5">
+            </div>
+        </div>
+        <div class="row">
+            <div class="mb-5 col-4">
+              <label class="form-label secret">Vias de rolo</label>
+              <input type="number" class="form-control" id="vias_rolo" value="1">
+            </div>
+            <div class="mb-5 col-4">
+              <label class="form-label">Quantidade rolo</label>
+              <input type="number" class="form-control" id="quantidade_por_rolo" value="200">
+            </div>
+            <div class="mb-5 col-4">
+              <label class="form-label">Metros por rolo</label>
+              <input type="number" class="form-control" id="metros_por_rolo" value="100">
+            </div>
+        </div>
+        <div class="row">
+            <a type="submit" class="btn btn-primary btn_codimarc" onclick="calcular_etiquetas()">Calcular</a>
+        </div>
+    </form>
+</div>
+
+<div class="container my_container" id="mostrar_resultado"></div>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+<script src="./js/javascript.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ js/javascript.js
@@ -0,0 +1,185 @@
+function calcular_etiquetas(){
+
+    clear_div();
+    /*
+    total_etiquetas
+    vias_cortante
+    vias_usadas
+    vias_rolo
+    quantidade_por_rolo*/
+
+    var total_etiquetas = document.getElementById('total_etiquetas').value; //total de etiquetas da OP
+    var vias_cortante = document.getElementById('vias_cortante').value; //vias do cortante
+    var vias_usadas = document.getElementById('vias_usadas').value; //vias que o operador pode usar consuante a largura da bobine
+    var vias_rolo = document.getElementById('vias_rolo').value; //vias de etiquetas por rolos
+    var quantidade_por_rolo = document.getElementById('quantidade_por_rolo').value; //quantidade de etiquetas por rolo
+    var metros_por_rolo = document.getElementById('metros_por_rolo').value; //quantidade de etiquetas por rolo
+    
+
+    //saber quantos rolos serão feitos de uma vez
+
+    var rolos_feitos_por_acao = vias_cortante/vias_rolo;
+
+    if(rolos_feitos_por_acao < 1 || vias_usadas>vias_cortante){
+        alert("Impossivel vias do cortante não são as suficientes");
+    }else{
+        //calcular numero de ações que o operador vai realizar
+        var numero_de_rolos_total = total_etiquetas/quantidade_por_rolo;
+        var numero_acoes = numero_de_rolos_total/rolos_feitos_por_acao;
+
+        //Number.isInteger(numero_acoes) == true quer dizer que rolos vão ser todos iguais
+        //Number.isInteger(numero_acoes) == false quer dizer que vão existir rolos mais pequenos
+        if(numero_acoes == 1){
+
+            document.getElementById("mostrar_resultado").replaceChildren();
+            document.getElementById("mostrar_resultado").innerHTML +=
+            '<div class="row">'+
+                '<table class="table">'+
+                    '<thead>'+
+                        '<tr>'+
+                            '<th scope="col">Numero de rolos</th>'+
+                            '<th scope="col">Quantidade por rolo</th>'+
+                            '<th scope="col">Metros</th>'+
+                        '</tr>'+
+                    '</thead>'+
+                    '<tbody>'+
+                        '<tr>'+
+                            '<td>'+numero_de_rolos_total+'</td>'+
+                            '<td>'+quantidade_por_rolo+'</td>'+
+                            '<td>'+metros_por_rolo+' metros</td>'+
+                        '</tr>'+
+                    '</tbody>'+
+                '</table>'+
+            '</div>'+
+            '<div class="row">'+
+                '<a type="submit" class="btn btn-primary btn_codimarc" onclick="clear_div()">Apagar</a>'+
+            '</div>';
+
+            console.log("Para produzir "+total_etiquetas+" etiquetas serão produzidos "+numero_de_rolos_total+" rolos com "+quantidade_por_rolo+" etiquetas em metros é "+metros_por_rolo);
+        }else if(numero_acoes < 1){
+            //exemplo
+            //1000 etiquetas total
+            //200 por rolo
+            //vias 6
+            var quantidade_por_rolo_incompleto = Math.trunc(total_etiquetas/rolos_feitos_por_acao)+1;
+            var metros_por_rolo_incompleto = Math-round((quantidade_por_rolo_incompleto*metros_por_rolo/quantidade_por_rolo),-3);
+
+            document.getElementById("mostrar_resultado").replaceChildren();
+            document.getElementById("mostrar_resultado").innerHTML +=
+            '<div class="row">'+
+                '<table class="table">'+
+                    '<thead>'+
+                        '<tr>'+
+                            '<th scope="col">Numero de rolos</th>'+
+                            '<th scope="col">Quantidade por rolo</th>'+
+                            '<th scope="col">Metros</th>'+
+                        '</tr>'+
+                    '</thead>'+
+                    '<tbody>'+
+                        '<tr>'+
+                            '<td>'+rolos_feitos_por_acao+'</td>'+
+                            '<td>'+quantidade_por_rolo_incompleto+'</td>'+
+                            '<td>'+metros_por_rolo_incompleto+' metros</td>'+
+                        '</tr>'+
+                    '</tbody>'+
+                '</table>'+
+            '</div>'+
+            '<div class="row">'+
+                '<a type="submit" class="btn btn-primary btn_codimarc" onclick="clear_div()">Apagar</a>'+
+            '</div>';
+
+            console.log("Para produzir "+total_etiquetas+" etiquetas serão produzidos "+rolos_feitos_por_acao+" rolos com "+quantidade_por_rolo_incompleto+" etiquetas em metros é "+metros_por_rolo_incompleto);
+        }else if(numero_acoes > 1){
+            //exemplo
+            //1000 etiquetas total
+            //200 por rolo
+            //vias 3
+            
+            //1 - calcular total etiquetas de rolos completos
+            var total_acoes_etiquetas_rolos_completos = Math.trunc(total_etiquetas/quantidade_por_rolo/rolos_feitos_por_acao);
+            console.log("1-calcular total etiquetas de rolos completos "+total_acoes_etiquetas_rolos_completos);
+            //2 - calcular numero de rolos ineiros
+            var total_rolos_inteiros = total_acoes_etiquetas_rolos_completos*rolos_feitos_por_acao;
+            console.log("2-calcular numero de rolos inteiros "+ total_rolos_inteiros);
+            //3 - calcular numeo de etiquetas feitas nos rolos inteiros
+            var total_etiquetas_nos_rolos_completos = total_rolos_inteiros*quantidade_por_rolo;
+            console.log("3 - calcular numeo de etiquetas feitas nos rolos inteiros "+ total_etiquetas_nos_rolos_completos);
+            //4 - calcular etiquetas que faltam para fazer nos rolos incompletos
+            var total_etiquetas_nos_rolos_incompletos = total_etiquetas-total_etiquetas_nos_rolos_completos;
+            console.log("4 - calcular etiquetas que faltam para fazer nos rolos incompletos "+total_etiquetas_nos_rolos_incompletos);
+            //5 - calcular quantas etiquetas leva cada rolo incompleto
+            var quantidade_por_rolo_incompleto = total_etiquetas_nos_rolos_incompletos/rolos_feitos_por_acao;
+            if(Number.isInteger(quantidade_por_rolo_incompleto) == false){
+                quantidade_por_rolo_incompleto =  Math.trunc(quantidade_por_rolo_incompleto)+1;
+            }
+            console.log("5 - calcular quantas etiquetas leva cada rolo incompleto "+quantidade_por_rolo_incompleto);
+            //6- calcular metros dos rolo incompleto
+            var metros_por_rolo_incompleto = Math.round((quantidade_por_rolo_incompleto*metros_por_rolo/quantidade_por_rolo),-3);
+            console.log("6- calcular metros dos rolo incompleto "+metros_por_rolo_incompleto);
+
+            document.getElementById("mostrar_resultado").replaceChildren();
+            document.getElementById("mostrar_resultado").innerHTML +=
+            '<div class="row">'+
+                '<table class="table">'+
+                    '<thead>'+
+                        '<tr>'+
+                            '<th scope="col">Numero de rolos</th>'+
+                            '<th scope="col">Quantidade por rolo</th>'+
+                            '<th scope="col">Metros</th>'+
+                        '</tr>'+
+                    '</thead>'+
+                    '<tbody>'+
+                        '<tr>'+
+                            '<td>'+total_rolos_inteiros+'</td>'+
+                            '<td>'+quantidade_por_rolo+'</td>'+
+                            '<td>'+metros_por_rolo+'</td>'+
+                        '</tr>'+
+                        '<tr>'+
+                            '<td>'+rolos_feitos_por_acao+'</td>'+
+                            '<td>'+quantidade_por_rolo_incompleto+'</td>'+
+                            '<td>'+metros_por_rolo_incompleto+' metros</td>'+
+                        '</tr>'+
+                    '</tbody>'+
+                '</table>'+
+            '</div>'+
+            '<div class="row">'+
+                '<a type="submit" class="btn btn-primary btn_codimarc" onclick="clear_div()">Apagar</a>'+
+            '</div>';
+
+            console.log("Para produzir "+total_etiquetas+" vão ser produzidos "+total_rolos_inteiros+" rolos com "+quantidade_por_rolo+
+            " mais "+rolos_feitos_por_acao+" rolos com "+quantidade_por_rolo_incompleto+" etiquetas em metros é "+metros_por_rolo_incompleto);
+            }
+        }
+    }
+
+function clear_div(){
+    document.getElementById("mostrar_resultado").replaceChildren();
+    document.getElementById("mostrar_resultado").innerHTML += '';
+}
+/*
+
+document.getElementById("mostrar_resultado").replaceChildren();
+            document.getElementById("content").innerHTML +=
+            '<div class="row">'+
+                '<table class="table">'+
+                    '<thead>'+
+                        '<tr>'+
+                            '<th scope="col">Numero de rolos</th>'+
+                            '<th scope="col">Quantidade por rolo</th>'+
+                            '<th scope="col">Metros</th>'+
+                        '</tr>'+
+                    '</thead>'+
+                    '<tbody>'+
+                        '<tr>'+
+                            '<td>50</td>'+
+                            '<td>100</td>'+
+                            '<td>500m</td>'+
+                        '</tr>'+
+                    '</tbody>'+
+                '</table>'+
+            '</div>'+
+            '<div class="row">'+
+                '<a type="submit" class="btn btn-primary btn_codimarc" onclick="">Apagar</a>'+
+            '</div>';
+
+*/
